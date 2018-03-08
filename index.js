5d2b09864b0b248ec5df93977d5546cd40bd62e7
P G <pgauchan@gmail.com>
2018-03-08 21:54:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 // write your code below!
+
+
+function happyHolidayTo (holiday, name) {
+	return `Happy ${holiday}, ${name}!`
+}
+
 function holidayCountdown(holiday, days) {
 	return `It's ${days} until ${holiday}!`
 }
