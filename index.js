15d323675f3866348a87eb5224790a5ce2901b31
P G <pgauchan@gmail.com>
2018-03-08 22:01:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,14 +2,3 @@ function happyHolidays() {
   return "Happy Holidays!"
 }
 
-function happyHolidaysTo(name) {
-  return `Happy Holidays, ${name}!
-}
-
-function happyHolidaysTo(name, holiday) {
-  return `Happy ${holiday}, ${name}!`
-}
-
-function holidayCountdown(holiday, days) {
-  return `It\'s ${days} days until ${holiday}!`
-}
