2c6b9c9ee2e32b202ce06fd9c313b687ebe62e29
P G <pgauchan@gmail.com>
2018-03-08 21:54:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 // write your code below!
 
 
+function happyHolidaysTo(name){
+	return `Happy holidays, ${name}!`
+}
+
 function happyHolidayTo (holiday, name) {
 	return `Happy ${holiday}, ${name}!`
 }
